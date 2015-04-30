34497dae4d4c4251f28f8913aef3928edd86ffc8
Jesper Wohlert <wohlert.jesper@gmail.com>
2015-04-30 08:44:23 +0200

    we can count

--- Encode.java
+++ Encode.java
@@ -12,13 +12,23 @@ public class Encode {
 
     // Table containing frequency of all occourences
     // ie. frequencies[24] = 4 indicates that the 24 occures 4 times
-    int[] frequencies = new int[255];
+    int[] frequencies = new int[256];    
 
     int bit;
-    while ( (bit = in.readBit()) != -1 )
-        frequencies[bit] += 1;
+    String bytelist = "";
+    while ( (bit = in.readBit()) != -1 ) {
+      bytelist += "" + bit;
+      if (bytelist.length() % 8 == 0) {
+        frequencies[Integer.parseInt(bytelist, 2)] += 1;
+        bytelist = "";
+      }      
+    }
 
-    in.close();
+    in.close();    
+
+    for (int i = 0; i < frequencies.length; i++) {      
+      System.out.println(frequencies[i] + "\t");
+    }
 
     huffman(frequencies);
 
@@ -26,6 +36,13 @@ public class Encode {
   }
 
   private static void huffman(int[] table) {
+    PQ pqHeap = new PQHeap(table.length);
+    DictBinTree tree = new DictBinTree();
 
+    for(int i = 0; i < table.length; i++) {
+      if(table[i] != 0) {
+        pqHeap.insert(new Element(table[i], tree.new Node(i)));
+      }
+    }
   }
 }
