18ddeebc2b78e87bd219769cbd259249fb0976d4
Jens Schwee <jensschwee@msn.com>
2015-05-05 13:52:14 +0200

    JavaDoc

--- Encode.java
+++ Encode.java
@@ -4,57 +4,57 @@ import java.io.IOException;
 
 public class Encode {
 
-  public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-    FileInputStream inFile = new FileInputStream(args[0]);
-    FileOutputStream outFile = new FileOutputStream(args[1]);
+        FileInputStream inFile = new FileInputStream(args[0]);
+        FileOutputStream outFile = new FileOutputStream(args[1]);
 
-    BitInputStream in = new BitInputStream(inFile);
-    BitOutputStream out = new BitOutputStream(outFile);
+        BitInputStream in = new BitInputStream(inFile);
+        BitOutputStream out = new BitOutputStream(outFile);
 
-    // 1. passthrough - build huffman tree and encoding
-    int bit;
-    int[] frequencies = new int[256];
-    String bytelist = "";
-    String[] encodedTable = null;
+        // 1. passthrough - build huffman tree and encoding
+        int bit;
+        int[] frequencies = new int[256];
+        String bytelist = "";
+        String[] encodedTable = null;
 
-    while ((bit = in.readBit()) != -1) {
-      bytelist += "" + bit;
-      if (bytelist.length() % 8 == 0) {
-        frequencies[Integer.parseInt(bytelist, 2)] += 1;
-        bytelist = "";
-      }
-    }
-
-    for (int i : frequencies) out.writeInt(i);
-
-    try {
-      Element e = Huffman.build(frequencies);
-      encodedTable = Huffman.encode((DictBinTree.Node) e.data);
-    } catch (NullPointerException ex) {
-      System.err.println("Error: Cannot encode an empty file.");
-    }
-
-    in.close();
-
-    // 2. passthrough - write code to file
-    inFile = new FileInputStream(args[0]);
-    in = new BitInputStream(inFile);
-
-
-    while ((bit = in.readBit()) != -1) {
-      bytelist += "" + bit;
-      if (bytelist.length() % 8 == 0) {
-        for (char c : encodedTable[Integer.parseInt(bytelist, 2)].toCharArray()) {
-          out.writeBit(Integer.parseInt(Character.toString(c)));
+        while ((bit = in.readBit()) != -1) {
+            bytelist += "" + bit;
+            if (bytelist.length() % 8 == 0) {
+                frequencies[Integer.parseInt(bytelist, 2)] += 1;
+                bytelist = "";
+            }
         }
-        bytelist = "";
-      }
+
+        for (int i : frequencies) out.writeInt(i);
+
+        try {
+            Element e = Huffman.build(frequencies);
+            encodedTable = Huffman.encode((DictBinTree.Node) e.data);
+        } catch (NullPointerException ex) {
+            System.err.println("Error: Cannot encode an empty file.");
+        }
+
+        in.close();
+
+        // 2. passthrough - write code to file
+        inFile = new FileInputStream(args[0]);
+        in = new BitInputStream(inFile);
+
+
+        while ((bit = in.readBit()) != -1) {
+            bytelist += "" + bit;
+            if (bytelist.length() % 8 == 0) {
+                for (char c : encodedTable[Integer.parseInt(bytelist, 2)].toCharArray()) {
+                    out.writeBit(Integer.parseInt(Character.toString(c)));
+                }
+                bytelist = "";
+            }
+        }
+
+        in.close();
+        out.close();
+
     }
-
-    in.close();
-    out.close();
-
-  }
 
 }
--- Huffman.java
+++ Huffman.java
@@ -4,97 +4,109 @@ import java.util.stream.*;
 
 public class Huffman {
 
-  private static DictBinTree tree = new DictBinTree();
+    private static DictBinTree tree = new DictBinTree();
 
-  /**
-   * Builds a new Huffman tree given a table of frequencies
-   *
-   * param table - an array of frequencies
-   * return - the root node of the Huffman tree
-   */
-  public static Element build(int[] table) {
-    int elementCount, i;
+    /**
+     * Builds a new Huffman tree given a table of frequencies
+     * <p>
+     * param table - an array of frequencies
+     * return - the root node of the Huffman tree
+     */
+    public static Element build(int[] table) {
+        int elementCount, i;
 
-    elementCount = (int) IntStream.of(table)
-      .filter(n -> n != 0)
-      .count();
+        elementCount = (int) IntStream.of(table)
+                .filter(n -> n != 0)
+                .count();
 
-    if (elementCount > 0) {
-      PQ pqHeap = new PQHeap(elementCount);
+        if (elementCount > 0) {
+            PQ pqHeap = new PQHeap(elementCount);
 
-      // Fill the heap with elementCount number of elements with
-      // the frequency as the element key and a childless node with the value
-      // of the character as the node key as the data.
-      IntStream.range(0, table.length)
-        .filter(n -> table[n] != 0)
-        .forEach(n -> pqHeap.insert(new Element(table[n], tree.new Node(n))));
+            // Fill the heap with elementCount number of elements with
+            // the frequency as the element key and a childless node with the value
+            // of the character as the node key as the data.
+            IntStream.range(0, table.length)
+                    .filter(n -> table[n] != 0)
+                    .forEach(n -> pqHeap.insert(new Element(table[n], tree.new Node(n))));
 
-      for (i = 0; i < elementCount - 1; i++) {
-        Element left, right;
-        int key;
+            for (i = 0; i < elementCount - 1; i++) {
+                Element left, right;
+                int key;
 
-        DictBinTree.Node node = tree.new Node(i);
+                DictBinTree.Node node = tree.new Node(i);
 
-        left = pqHeap.extractMin();
-        right = pqHeap.extractMin();
+                left = pqHeap.extractMin();
+                right = pqHeap.extractMin();
 
-        node.left = (DictBinTree.Node) left.data;
-        node.right = (DictBinTree.Node) right.data;
+                node.left = (DictBinTree.Node) left.data;
+                node.right = (DictBinTree.Node) right.data;
 
-        key = left.key + right.key;
+                key = left.key + right.key;
 
-        node.key = key;
+                node.key = key;
 
-        pqHeap.insert(new Element(key, node));
-      }
+                pqHeap.insert(new Element(key, node));
+            }
 
-      return pqHeap.extractMin();
+            return pqHeap.extractMin();
+        }
+        return null;
     }
-    return null;
-  }
 
-  /**
-   * Uses Huffman coding to encode a Huffman tree
-   *
-   * param root - the rootnode of the Huffman tree
-   * return - a table of encoded values
-   */
-  public static String[] encode(DictBinTree.Node root) {
-    String[] table = new String[256];
-    recursiveEncode(root, table, "");
-    return table;
-  }
-
-  public static Map<String,Integer> decode(DictBinTree.Node root) {
-    Map<String, Integer> table = new HashMap<String, Integer>();
-    recursiveDecode(root, table, "");
-    return table;
-  }
-
-  private static void recursiveDecode(DictBinTree.Node node, Map<String,Integer> table, String bitString) {
-    if (node == null) return;
-    if (node.left == null && node.right == null) {
-      table.put(bitString, node.key);
-    } else {
-      recursiveDecode(node.left, table, bitString + "0");
-      recursiveDecode(node.right, table, bitString + "1");
+    /**
+     * Uses Huffman coding to encode a Huffman tree
+     * <p>
+     * param root - the rootnode of the Huffman tree
+     * return - a table of encoded values
+     */
+    public static String[] encode(DictBinTree.Node root) {
+        String[] table = new String[256];
+        recursiveEncode(root, table, "");
+        return table;
     }
-  }
 
-  /**
-   * Recursively generates bitstrings based on a Huffman tree.
-   *
-   * param node - current node that is being proccessed
-   * param table - the table to insert strings into
-   * param bitString - the string to insert into the table
-   */
-  private static void recursiveEncode(DictBinTree.Node node, String[] table, String bitString) {
-    if (node == null) return;
-    if (node.left == null && node.right == null) {
-      table[node.key] = bitString;
-    } else {
-      recursiveEncode(node.left, table, bitString + "0");
-      recursiveEncode(node.right, table, bitString + "1");
+    /**
+     * Uses Huffman coding to decode a Huffman tree
+     * param root - the rootnode of the Huffman tree
+     * return - a HashMap of decode values
+     */
+    public static Map<String, Integer> decode(DictBinTree.Node root) {
+        Map<String, Integer> table = new HashMap<String, Integer>();
+        recursiveDecode(root, table, "");
+        return table;
     }
-  }
+
+    /**
+     * Recursively generates bitstrings based on a Huffman tree.
+     *
+     * @param node - current node that is being proccessed
+     *             param table - the table to insert strings into
+     *             param bitString - the string to insert into the table
+     */
+    private static void recursiveDecode(DictBinTree.Node node, Map<String, Integer> table, String bitString) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            table.put(bitString, node.key);
+        } else {
+            recursiveDecode(node.left, table, bitString + "0");
+            recursiveDecode(node.right, table, bitString + "1");
+        }
+    }
+
+    /**
+     * Recursively generates bitstrings based on a Huffman tree.
+     * <p>
+     * param node - current node that is being proccessed
+     * param table - the table to insert strings into
+     * param bitString - the string to insert into the table
+     */
+    private static void recursiveEncode(DictBinTree.Node node, String[] table, String bitString) {
+        if (node == null) return;
+        if (node.left == null && node.right == null) {
+            table[node.key] = bitString;
+        } else {
+            recursiveEncode(node.left, table, bitString + "0");
+            recursiveEncode(node.right, table, bitString + "1");
+        }
+    }
 }
